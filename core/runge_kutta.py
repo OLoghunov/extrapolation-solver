364427185c70feee5152c5843b1d14c54eb3bdc6
OLoghunov <loghunov1@gmail.com>
2025-05-04 16:23:26 +0300

    implemented rk2 and rk4 methods

---
+++ core/runge_kutta.py
@@ -0,0 +1,36 @@
+import numpy as np
+from .base_solver import BaseODESolver
+
+class RungeKutta4(BaseODESolver):
+    """RK4 method."""
+    
+    def solve(self):
+        t = np.arange(self.t0, self.tf + self.h, self.h)
+        y = np.zeros(len(t))
+        y[0] = self.y0
+        
+        for i in range(len(t) - 1):
+            h = t[i+1] - t[i]
+            k1 = h * self.f(t[i], y[i])
+            k2 = h * self.f(t[i] + h/2, y[i] + k1/2)
+            k3 = h * self.f(t[i] + h/2, y[i] + k2/2)
+            k4 = h * self.f(t[i] + h, y[i] + k3)
+            y[i+1] = y[i] + (k1 + 2*k2 + 2*k3 + k4) / 6
+        
+        return t, y
+
+class RungeKutta2(BaseODESolver):
+    """RK2 method."""
+    
+    def solve(self):
+        t = np.arange(self.t0, self.tf + self.h, self.h)
+        y = np.zeros(len(t))
+        y[0] = self.y0
+        
+        for i in range(len(t) - 1):
+            h = t[i+1] - t[i]
+            k1 = h * self.f(t[i], y[i])
+            k2 = h * self.f(t[i] + h, y[i] + k1)
+            y[i+1] = y[i] + (k1 + k2) / 2
+        
+        return t, y
