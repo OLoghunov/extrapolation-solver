aad39cd3d9e075e15d55afb9d3f20b377e65e7fa
OLoghunov <loghunov1@gmail.com>
2025-05-10 17:45:33 +0300

    made AB2 and AB4 pure multistep methods, and bootstrap — external.

--- core/multistep.py
+++ core/multistep.py
@@ -3,44 +3,42 @@ from .base_solver import BaseODESolver
 from .runge_kutta import RungeKutta2, RungeKutta4
 
 class AdamsBashforth2(BaseODESolver):
-    """Adams-Bashforth second order method with RK2 bootstrap."""
-    
+    def __init__(self, f, t_eval, y_boot):
+        super().__init__(f, [t_eval[0], t_eval[-1]], y_boot[0], t_eval[1] - t_eval[0])
+        self.t_eval = t_eval
+        self.y_boot = y_boot
+
     def solve(self):
-        t = np.arange(self.t0, self.tf + self.h, self.h)
+        t = self.t_eval
         y = np.zeros((len(t), len(self.y0)))
-        y[0] = self.y0
-        
-        rk_solver = RungeKutta2(self.f, [t[0], t[1]], y[0], h=self.h)
-        _, y_rk = rk_solver.solve()
-        y[1] = y_rk[-1]
-        
-        # Main cycle
+        y[:2] = self.y_boot[:2]
+
         for i in range(1, len(t) - 1):
             h = t[i+1] - t[i]
-            y[i+1] = y[i] + h * (3/2 * self.f(t[i], y[i]) - 1/2 * self.f(t[i-1], y[i-1]))
-        
+            y[i+1] = y[i] + h * (
+                3/2 * self.f(t[i], y[i]) - 1/2 * self.f(t[i-1], y[i-1])
+            )
+
         return t, y
 
 class AdamsBashforth4(BaseODESolver):
-    """Adams-Bashforth forth order method with RK4 bootstrap."""
-    
+    def __init__(self, f, t_eval, y_boot):
+        super().__init__(f, [t_eval[0], t_eval[-1]], y_boot[0], t_eval[1] - t_eval[0])
+        self.t_eval = t_eval
+        self.y_boot = y_boot
+
     def solve(self):
-        t = np.arange(self.t0, self.tf + self.h, self.h)
+        t = self.t_eval
         y = np.zeros((len(t), len(self.y0)))
-        y[0] = self.y0
-        
-        rk_solver = RungeKutta4(self.f, t[:4], y[0], h=self.h)
-        _, y_rk = rk_solver.solve()
-        y[1:4] = y_rk[1:4]
-        
-        # Main cycle
+        y[:4] = self.y_boot[:4]
+
         for i in range(3, len(t) - 1):
             h = t[i+1] - t[i]
             y[i+1] = y[i] + h * (
-                55/24 * self.f(t[i], y[i]) 
-                - 59/24 * self.f(t[i-1], y[i-1]) 
-                + 37/24 * self.f(t[i-2], y[i-2]) 
+                55/24 * self.f(t[i], y[i])
+                - 59/24 * self.f(t[i-1], y[i-1])
+                + 37/24 * self.f(t[i-2], y[i-2])
                 - 9/24 * self.f(t[i-3], y[i-3])
             )
-        
+
         return t, y
