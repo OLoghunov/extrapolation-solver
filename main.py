0acd5704200849d8582f3c7ccf0cf800bb06fdf2
OLoghunov <loghunov1@gmail.com>
2025-05-18 04:31:21 +0300

    added multiprocessing for optimization

--- main.py
+++ main.py
@@ -1,46 +1,41 @@
-import numpy as np
-from core.runge_kutta import RungeKutta4
+from multiprocessing import Pool, set_start_method
+from experiments.calcs import compute_rk4_error, compute_esimm_error
 from core.esimm_solver import ESIMMSolver
-from accuracy.error_estimator import compute_error, estimate_order
+from accuracy.error_estimator import estimate_order
 from accuracy.benchmarks import benchmarks
 from visualization import plot_errors
 
-# Config
-h_list = [0.4, 0.2, 0.1, 0.05]
-extrapolation_seq = [1, 2, 4]
-problem = benchmarks["lorenz"]
+# Configuration
+h_list = [1.6, 0.8, 0.4, 0.2, 0.1, 0.05]
+extrapolation_seq = [1, 2, 6]
+problem_name = "lorenz"
 
+problem = benchmarks[problem_name]
 ode = problem["ode"]
 exact = problem["exact"]
 y0 = problem["y0"]
 t_span = problem["t_span"]
 
-# Error estimation RK4
-rk4_errors = []
-for h in h_list:
-    t_eval = np.arange(t_span[0], t_span[1] + h, h)
-    solver = RungeKutta4(ode, t_eval, y0, h=h)
-    t, y = solver.solve()
-    y_true = exact(t)
-    rk4_errors.append(compute_error(y, y_true))
 
-# Error estimation ESIMM
-esimm_errors = []
-for h in h_list:
-    t_eval = np.arange(t_span[0], t_span[1] + h, h)
-    solver = ESIMMSolver(ode, t_eval, y0, extrapolation_seq, h=h)
-    t, y = solver.solve()
-    y_true = exact(t)
-    esimm_errors.append(compute_error(y, y_true))
+def rk4_wrapper(h):
+    return compute_rk4_error(problem_name, h)
 
-# ESIMM order estimation
-esimm_order = estimate_order(
-    ode, t_span, y0, exact, 
-    ESIMMSolver, h_list,
-    extrapolation_seq=extrapolation_seq
-)
 
-print(f"ESIMM estimated order: {esimm_order:.2f}")
+def esimm_wrapper(h):
+    return compute_esimm_error(problem_name, extrapolation_seq, h)
 
-# Plot
-plot_errors(h_list, rk4_errors, esimm_errors)
+
+if __name__ == "__main__":
+    set_start_method("spawn", force=True)
+
+    with Pool() as pool:
+        rk4_errors = pool.map(rk4_wrapper, h_list)
+        esimm_errors = pool.map(esimm_wrapper, h_list)
+
+    esimm_order = estimate_order(
+        ode, t_span, y0, exact, ESIMMSolver, h_list, extrapolation_seq=extrapolation_seq
+    )
+
+    print(f"ESIMM estimated order: {esimm_order:.2f}")
+
+    plot_errors(h_list, rk4_errors, esimm_errors)
